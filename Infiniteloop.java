e4f68eba5119e367b93f7e2b7f1dfcbdeae0c194
HeartSlayer0 <104336565+HeartSlayer0@users.noreply.github.com>
2023-02-25 17:11:38 +0530

    Add files via upload

---
+++ Infiniteloop.java
@@ -0,0 +1,15 @@
+package prac;
+
+public class Infiniteloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=0;
+		int a=0;
+		while(n!=1) {
+			a++;
+			System.out.println(a);
+		}
+	}
+
+}
